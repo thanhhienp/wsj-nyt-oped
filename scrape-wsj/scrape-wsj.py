0dda81f409c89a00160c50a0926c3ea8bc1a3865
thanhhienp <66493400+thanhhienp@users.noreply.github.com>
2021-04-15 21:13:51 -0400

    scraping wsj at day level

--- scrape-wsj/scrape-wsj.py
+++ scrape-wsj/scrape-wsj.py
@@ -1,5 +1,4 @@
 # Script to scrape the Wall Street Journal op-ed section
-# To see the scraping structure, check README.md
 import pandas as pd
 import argparse
 from bs4 import BeautifulSoup
@@ -16,12 +15,36 @@ password = args.password
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36'}
 
 # List of article types that should be scraped
-articleType = ['Commentary']
-url = 'https://www.wsj.com/articles/the-j-j-covid-vaccine-pause-11618354443'
+articleTypeList = ['Commentary', 'Review & Outlook', 'Wonder Land']
 
-page = requests.get(url, headers=headers)
-soup = BeautifulSoup(page.content, 'html.parser')
+# url for scraping
+testurl = 'https://www.wsj.com/news/archive/2021/01/01'
 
-article_content = soup.find('div', class_='article-content')
-article_text = [s.get_text() for s in article_content.find_all('p')]
-print(article_text)
+# Retrieve the page
+dayPage = requests.get(testurl, headers=headers)
+daySoup = BeautifulSoup(dayPage.content, 'html.parser')
+
+# Retrieve page main contents, list of flashlines (article types) and headlines
+dailyMain = daySoup.main
+flashlines = dailyMain.find_all('div', class_='WSJTheme--articleType--34Gt-vdG')
+headlines = dailyMain.find_all('div', class_='WSJTheme--headline--7VCzo7Ay')
+
+# Empty list for article information
+opinion = {'Headline': [], 'URL': [], 'Type': [], 'Text': []}
+
+# Get list of opinion articles
+for idx in range(len(flashlines)):
+    type = flashlines[idx].span.get_text()
+    if type in articleTypeList:
+        opinion['Headline'].append(headlines[idx].a.get_text())
+        opinion['URL'].append(headlines[idx].a.get('href'))
+        opinion['Type'].append(type)
+
+# parse content within an opinion page
+for url in opinion['URL']:
+    opinionPage = requests.get(url, headers=headers)
+    opinionSoup = BeautifulSoup(opinionPage.content, 'html.parser')
+    opinionContent = opinionSoup.find('div', class_='article-content')
+    opinion['Text'] = [s.get_text() for s in opinionContent.find_all('p')]
+
+print(opinion)
